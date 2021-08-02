f7b67e57ea426d1a3b80c0078bd68f8455f3f1dd
Long Ngo <ngobi0617@gmail.com>
2021-08-02 22:30:39 +0700

    upload

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <div class="game-container">
+    <Figure :wrong-count="wrongLetters.length" />
+    <WrongLetters :wrong-letters="wrongLetters" />
+    <Word :letters="letters" :correct-letters="correctLetters" />
+  </div>
+  <Popup :status="status" :word="word" @reset="reset" />
+  <Notification :show="notification" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import "./assets/style.css";
+import { computed, ref } from "vue";
+import onKeydown from "./assets/onKeydown";
+
+import Header from "@/components/Header";
+import Figure from "@/components/Figure";
+import WrongLetters from "@/components/WrongLetters";
+import Word from "@/components/Word";
+import Popup from "@/components/Popup";
+import Notification from "@/components/Notification";
+
+const words = ["programming", "vuejs", "composition"];
+const randomWord = () => words[Math.floor(Math.random() * words.length)];
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  components: { Header, Figure, Word, WrongLetters, Popup, Notification },
+  setup() {
+    const word = ref(randomWord());
+    const guessedLetters = ref([]);
+
+    const letters = computed(() => word.value.split(""));
+    const wrongLetters = computed(() =>
+      guessedLetters.value.filter((l) => !letters.value.includes(l))
+    );
+    const correctLetters = computed(() =>
+      guessedLetters.value.filter((l) => letters.value.includes(l))
+    );
+
+    const status = computed(() => {
+      if (wrongLetters.value.length === 6) return "lose";
+      if (letters.value.every((l) => correctLetters.value.includes(l)))
+        return "win";
+      return "";
+    });
+
+    const reset = () => {
+      guessedLetters.value = [];
+      word.value = randomWord();
+    };
+
+    const notification = ref(false);
+    const showNotification = () => {
+      notification.value = true;
+      setTimeout(() => (notification.value = false), 2000);
+    };
+
+    onKeydown((event) => {
+      const letter = event.key.toLowerCase();
+      if (event.keyCode < 65 && event.keyCode > 90) return;
+      if (status.value) return;
+      if (guessedLetters.value.includes(letter)) {
+        showNotification();
+        return;
+      }
+      guessedLetters.value.push(letter);
+    });
+    
+    return {
+      letters,
+      word,
+      wrongLetters,
+      correctLetters,
+      guessedLetters,
+      notification,
+      status,
+      reset,
+    };
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
---
+++ src/components/Header.vue
@@ -0,0 +1,5 @@
+<template>
+ <h1>Hangman</h1>
+  <p>Find the hidden word - Enter a letter</p>
+</template>
+
